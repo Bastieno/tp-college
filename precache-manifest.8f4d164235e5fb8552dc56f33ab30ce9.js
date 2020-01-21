5e06443dcc59f56df0cdd0906066835b6c058543
Francis Nduamaka <francis.nduamaka@gmail.com>
2020-01-21 17:52:40 +0100

    Updates

---
+++ precache-manifest.8f4d164235e5fb8552dc56f33ab30ce9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3b7a94a083b020e8381b10cfc0c99c",
+    "url": "/tp-college/index.html"
+  },
+  {
+    "revision": "f0be1fc8cbb1918ffef9",
+    "url": "/tp-college/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5febbf67a0433835fc8e",
+    "url": "/tp-college/static/css/main.50d1d700.chunk.css"
+  },
+  {
+    "revision": "f0be1fc8cbb1918ffef9",
+    "url": "/tp-college/static/js/2.02f98b63.chunk.js"
+  },
+  {
+    "revision": "639cb9b75c446d796a08fe6b7ddd9583",
+    "url": "/tp-college/static/js/2.02f98b63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5febbf67a0433835fc8e",
+    "url": "/tp-college/static/js/main.79e1d0e2.chunk.js"
+  },
+  {
+    "revision": "8bea7d9c35f3ee6d6441",
+    "url": "/tp-college/static/js/runtime-main.b08b9c6c.js"
+  }
+]);
